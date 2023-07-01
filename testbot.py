569a65ff454244dc42972ef0e8b1f599e9a25ffb
Long-Trinh-Tien <113412392+Long-Trinh-Tien@users.noreply.github.com>
2023-07-01 20:22:21 +0700

    first commit

---
+++ testbot.py
@@ -0,0 +1,17 @@
+import telebot
+
+# Khởi tạo bot
+bot = telebot.TeleBot("6342667549:AAGXrMmbZ5zcQs1B1s0V3KxPBrQmT2eN45Q")
+
+# Xử lý tin nhắn "/start"
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Xin chào! Tôi là bot.")
+
+# Xử lý tin nhắn "/send"
+@bot.message_handler(commands=['send'])
+def send_message(message):
+    bot.reply_to(message, "Đã gửi tin nhắn.")
+
+# Lắng nghe và xử lý tin nhắn mới
+bot.polling()
